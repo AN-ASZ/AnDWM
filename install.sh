198a1c4f97ecf4c9ea28b56146a223dac7c63643
AN A ZU <Anas7487@outlook.com>
2025-12-21 17:55:33 +0700

    hotfix

--- install.sh
+++ install.sh
@@ -35,7 +35,7 @@ sudo pacman -S --needed --noconfirm \
     kitty rofi flameshot nemo zig libc++ pam libxcb xcb-util picom \
     base-devel xorgproto libx11 libxext libxrandr libxinerama libxrender libxft \
     libxfixes libxdamage libxcomposite libxmu libxtst p7zip feh polkit-gnome \
-    wireless_tools xorg-xsetroot wget xorg-server xorg-xinit xorg-xrandr xorg-xset
+    wireless_tools xorg-xsetroot wget xorg-server xorg-xinit xorg-xrandr xorg-xset xterm iw
 yay -S --needed --noconfirm zen-browser-bin xkblayout-state-git
 
 echo "==> Installing cursor..."
