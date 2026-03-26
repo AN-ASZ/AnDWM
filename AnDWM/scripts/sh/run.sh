c80ffe39a69b311d90f66d79824e196b118d4f41
AN A ZU <Anas7487@outlook.com>
2026-03-26 15:04:06 +0700

    recompose start.sh

--- AnDWM/scripts/sh/run.sh
+++ AnDWM/scripts/sh/run.sh
@@ -3,40 +3,54 @@ export XDG_SESSION_TYPE=x11
 export DESKTOP_SESSION=dwm
 export XDG_CURRENT_DESKTOP=dwm
 
-# Ensure systemd user services (like portal) get DISPLAY/XAUTHORITY
+systemctl --user import-environment DISPLAY XAUTHORITY
 dbus-update-activation-environment --systemd DISPLAY XAUTHORITY
 
 xrdb merge ~/.Xresources
-#xbacklight -set 10 &
 
-#config your display here
-if xrandr | grep -q "HDMI-A-0 connected"; then
-    xrandr --output HDMI-A-0 --mode 1920x1080 --rate 100 --rotate inverted
-    xset s off
-    xset s noblank
-    xset -dpms
-fi
 
+########## FOR MULTI MONITOR IN VERTICAL SETUP ############
+##
+##xrandr \
+##  --output HDMI-1 --mode 1920x1080 --rate 100 --rotate inverted --pos 0x768 \
+##  --output eDP-1 --mode 1366x768 --pos 277x0 --primary
+###########################################################
+
+########## LIVE WALLPAPER ############
+#xwinwrap -ni -ov -fs -s -- \
+#sh -c 'mpv --wid=$1 --loop=inf --no-audio --no-border --hwdec=auto wallpaper.mp4' _ WID
+######################################
+
+########## COMMENT THIS IF USE LIVE WALLPAPER ############
 feh --bg-fill ~/.config/AnDWM/Wallpaper/Miku_plant.png
+###########################################################
+
 xset r rate 200 20 &
 
-sudo ~/.config/AnDWM/scripts/bin/isolate &
-
-wired &
-~/.config/AnDWM/scripts/bin/bar &
+# Notification Daemon
+taskset -c 0 wired &
+#bar
+taskset -c 0 ~/.config/AnDWM/scripts/bin/bar &
+# Compositor
 picom --config ~/.config/AnDWM/scripts/picom.conf &
 
+# ClipBoard
 greenclip daemon &
+
+# Language Layout switch key
 setxkbmap -layout us,th -option grp:win_space_toggle &
 
+# User Authentication prompt
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 
-# XDG autostart (Steam checkbox, etc.)
+# XDG autostart (Steam, Discord, etc.)
 dex -a -s ~/.config/autostart >/dev/null 2>&1 &
 
-~/.config/AnDWM/scripts/bin/bat &
+#taskset -c 0
+taskset -c 0 ionice -c3 nice -n 19 cpulimit -l 1 -- bash -c '~/.config/AnDWM/scripts/bin/bat' > /dev/null 2>&1 &
 
-#systemctl --user start opentabletdriver &
+#sudo ~/.config/AnDWM/scripts/sh/asusprofile.sh &
+taskset -c 0 ionice -c3 nice -n 19 cpulimit -l 5 -- bash -c 'sudo ~/.config/AnDWM/scripts/sh/asusprofile.sh' > /dev/null 2>&1 &
 
-# Start DWM inside correct dbus session
+# Start actual WM
 exec AnDWM
